03d972e96a9bc397923c166c698c9134d1f28602
Gerard Adamiak <134928600+GerardAdamiak@users.noreply.github.com>
2024-03-17 12:40:43 +0100

    animacje postaci poprawione

--- Trybiki/Assets/Scripts/strzelaczMove.cs
+++ Trybiki/Assets/Scripts/strzelaczMove.cs
@@ -18,7 +18,7 @@ public class MoveTowardsPlayer : MonoBehaviour
         float distanceToPlayer = Vector3.Distance(transform.position, player.position);
 
         // Check if the distance is greater than 40 units
-        if (distanceToPlayer > 5f)
+        if (distanceToPlayer > 4f)
         {
             // Move towards the player
             transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
